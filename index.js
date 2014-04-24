736a530331449c375df6a9c89b1800199b38d0ca
Eli Yukelzon <reflog@gmail.com>
2014-04-24 14:23:56 +0300

    initial import

---
+++ index.js
@@ -0,0 +1,48 @@
+"use strict";
+var through = require('through');
+var path = require('path');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+var File = gutil.File;
+
+module.exports = function (fileName, converter) {
+  if (!fileName) {
+    throw new PluginError('gulp-jsoncombine', 'Missing fileName option for gulp-jsoncombine');
+  }
+  if (!converter) {
+    throw new PluginError('gulp-jsoncombine', 'Missing converter option for gulp-jsoncombine');
+  }
+
+  var data = {};
+  var firstFile = null;
+
+  function bufferContents(file) {
+    if (!firstFile) {
+      firstFile = file;
+    }
+
+    if (file.isNull()) {
+      return; // ignore
+    }
+    if (file.isStream()) {
+      return this.emit('error', new PluginError('gulp-jsoncombine', 'Streaming not supported'));
+    }
+    data[file.relative.substr(0,file.relative.length-5)] = JSON.parse(file.contents.toString());
+  }
+
+  function endStream() {
+    var joinedPath = path.join(firstFile.base, fileName);
+
+    var joinedFile = new File({
+      cwd: firstFile.cwd,
+      base: firstFile.base,
+      path: joinedPath,
+      contents: converter(data)
+    });
+
+    this.emit('data', joinedFile);
+    this.emit('end');
+  }
+
+  return through(bufferContents, endStream);
+};
